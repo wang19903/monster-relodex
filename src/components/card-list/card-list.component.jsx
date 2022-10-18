01b13b9ee9906fef123f76e36025328770d9008b
wangg <lf21715@yahoo.com.tw>
2022-10-18 22:47:51 +0800

    18

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,7 +3,7 @@ import Card from "../card/card.component";
 const Cardlist = ({ monsters }) => (
   <div className="card-list">
     {monsters.map((monster) => {
-      return <Card monster={monster} />;
+      return <Card monster={monster} key={monster.id} />;
     })}
   </div>
 );
